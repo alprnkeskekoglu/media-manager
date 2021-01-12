4426e4d95fe61484050f9a16c7b238786594eb6b
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2021-01-12 23:11:57 +0300

    Initial Commit

---
+++ src/FileManagerServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Dawnstar\FileManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class FileManagerServiceProvider extends ServiceProvider
+{
+
+
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
+
