ce96e95e9aee48be5c6a09245903135e084ef9c4
alprnkeskekoglu <alperen.keskekoglu@mediaclick.com.tr>
2021-10-24 20:46:18 +0300

    v2 start

---
+++ src/MediaManagerServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Dawnstar\FileManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class MediaManagerServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
+
